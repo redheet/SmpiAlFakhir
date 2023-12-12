24b20b68b2164c554bbeba3b03de85b18c3c157a
redheet <naufalzidane32@gmail.com>
2023-12-12 14:09:53 +0700

    add insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Cta from "./components/layout/cta";
 import HeroSection from "./components/layout/hero-section";
 import NavMenu from "./components/layout/nav-menu";
-// import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <>
-      {/* <SpeedInsights/> */}
+      <SpeedInsights/>
       <NavMenu />
       <HeroSection />
       <Cta />
