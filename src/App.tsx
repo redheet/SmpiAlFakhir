08a6df8aa66537154bbd632062a08700e6f858e6
redheet <naufalzidane32@gmail.com>
2023-12-13 18:01:04 +0700

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import Router from './components/Router';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/home';
+import About from './components/pages/about';
+
 function App() {
   return (
     <>
-      <Router />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<About />} />
+        </Routes>
+        <SpeedInsights />
+      </BrowserRouter>
     </>
   );
 }
