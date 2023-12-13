30cc420a233590d8240c71bd6c6a83e1e1ec6115
redheet <naufalzidane32@gmail.com>
2023-12-13 17:37:16 +0700

    change

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,6 +2,7 @@ import { SpeedInsights } from '@vercel/speed-insights/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+
 function Router() {
   return (
     <>
