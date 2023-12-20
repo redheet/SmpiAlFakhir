b56d86c4821f882d40ba975f7349c8a2ff6efe21
redheet <naufalzidane32@gmail.com>
2023-12-20 12:18:58 +0700

    comented usestate

--- src/components/layout/form.tsx
+++ src/components/layout/form.tsx
@@ -1,6 +1,6 @@
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 // import {
 //   Select,
