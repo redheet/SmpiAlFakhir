d329c614d2a1d22aecac9d2a2b8d500e6511b3ec
redheet <naufalzidane32@gmail.com>
2023-12-21 14:02:58 +0700

    v-2'2

--- src/components/layout/nav-menu.tsx
+++ src/components/layout/nav-menu.tsx
@@ -82,26 +82,26 @@ const fasilitas2 = [
 //   { name: 'Guides', href: '#', icon: BookmarkSquareIcon },
 //   { name: 'Webinars', href: '#', icon: ComputerDesktopIcon }
 // ];
-const blogPosts = [
-  {
-    id: 1,
-    name: 'Boost your conversion rate',
-    href: '#',
-    preview:
-      'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1558478551-1a378f63328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80'
-  },
-  {
-    id: 2,
-    name: 'How to use search engine optimization to drive traffic to your site',
-    href: '#',
-    preview:
-      'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2300&q=80'
-  }
-];
+// const blogPosts = [
+//   {
+//     id: 1,
+//     name: 'Boost your conversion rate',
+//     href: '#',
+//     preview:
+//       'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1558478551-1a378f63328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80'
+//   },
+//   {
+//     id: 2,
+//     name: 'How to use search engine optimization to drive traffic to your site',
+//     href: '#',
+//     preview:
+//       'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2300&q=80'
+//   }
+// ];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
