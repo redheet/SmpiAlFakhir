f1cdb039ee464c0f45587655c97c8b400f491551
redheet <naufalzidane32@gmail.com>
2024-02-15 14:54:40 +0700

    add meta tag

--- src/components/ui/Head.tsx
+++ src/components/ui/Head.tsx
@@ -7,6 +7,11 @@ function Head({ title }) {
       <HelmetProvider>
         <Helmet>
           <title>{title} - SMPI Modern Al-Fakhir</title>
+          <meta
+            name="description"
+            content="SMP Islam yang mengusung konsep modern, mengadopsi model kelas dan program internasional, namun tetap mengutamakan pendidikan karakter, moral, akhlak dan nilai-nilai keislaman."
+          />
+          <meta property="og:site_name" content="School Center" />
           <link rel="shortcut icon" href={icon} />
         </Helmet>
       </HelmetProvider>
